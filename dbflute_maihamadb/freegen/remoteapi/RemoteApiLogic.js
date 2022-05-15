feae05c64f7c96a8caeaacc46f8ee0ffa48f5341
p1us2er0 <p1us3inus2er0@gmail.com>
2022-05-15 23:18:22 +0900

    refactoring ControlFreeGenRemoteApiJava.vm #7

--- dbflute_maihamadb/freegen/remoteapi/RemoteApiLogic.js
+++ dbflute_maihamadb/freegen/remoteapi/RemoteApiLogic.js
@@ -12,7 +12,7 @@ var remoteApiLogic = {
      * Return indent.
      * indent has 4 spaces.
      * @param {number} indentSize indent size.
-     * @return indent. (NotNull)
+     * @return {string} indent. (NotNull)
      */
     indent: function(indentSize) {
         // #for_now I want to set the initial value of indent to 0 size. by p1us2er0 (2022/05/04)
@@ -26,14 +26,14 @@ var remoteApiLogic = {
     },
 
     /**
-     * Return import declaration.
-     * Unique and sort importClassList. And return the import declaration separated by top package.
-     * @param {string[]} importClassList The list of import class.
+     * Optimizes and returns the import class list.
+     * Unique and sort import class list. And return the import class list separated by top package.
+     * @param {string[]} importClassList The list of import class(with package).
      * @param {string} currentPackage Package name of the class to declare import.
-     * @param {string[]} importTopPackageOrderList The order list of top packages of import declaration.
-     * @return import declaration. (NotNull)
+     * @param {string[]} importTopPackageOrderList The order list of top packages of import.
+     * @return {string[][]} import class list separated by top package. (NotNull)
      */
-    importDeclaration: function(importClassList, currentPackage, importTopPackageOrderList) {
+    optimizeImportClassList: function(importClassList, currentPackage, importTopPackageOrderList) {
         // Unique importClass.
         var uniqueImportClassList = new java.util.ArrayList(new java.util.HashSet(importClassList));
 
@@ -72,7 +72,7 @@ var remoteApiLogic = {
         });
 
         // Assemble an import declaration.
-        var importDeclaration = '';
+        var optimizedImportClassList = [];
         var keepTopPackage = '';
         uniqueImportClassList.forEach(function(importClass) {
             // If importClass is the same as currentPackage, skip. (Because no import declaration is required).
@@ -83,11 +83,11 @@ var remoteApiLogic = {
             // If the top Package changes, insert a blank line.
             if (importClass.substring(0, importClass.indexOf('.')) != keepTopPackage) {
                 keepTopPackage = importClass.substring(0, importClass.indexOf('.'));
-                importDeclaration += '\n';
+                optimizedImportClassList.push([]);
             }
-            importDeclaration += 'import ' + importClass + ';\n';
+            optimizedImportClassList[optimizedImportClassList.length - 1].push(importClass);
         });
 
-        return importDeclaration;
+        return optimizedImportClassList;
     }
 };
