dda05681620cdee881014aa35bfddb9c212d5cd8
jflute <jflute@localhost>
2026-04-07 00:13:36 +0900

    fieldName と fieldNamingMapping の関係性を整理整頓 #31

--- src/main/resources/remoteapi/schema/tricky/remoteapi_schema_tricky_rule.js
+++ src/main/resources/remoteapi/schema/tricky/remoteapi_schema_tricky_rule.js
@@ -1,21 +1,13 @@
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // RemoteApiGen your rule settings as ECMAScript5 (related to RemoteApiRule.js in freegen)
 // _/_/_/_/_/_/_/_/_/_/
-// =======================================================================================
-//                                                                                  Option
-//                                                                                  ======
-//
-// -----------------------------------------------------
-//                                          Field Naming
-//                                          ------------
-// test of no case conversion
-// #hope only specify at /fieldcase/noconv/ by jflute
-// @Override
-remoteApiRule.fieldNamingMapping = function() {
-  return { // empty means no conversion at all parts
-  };
-}
 
+// ===================================================================================
+//                                                                  Param/Return(base)
+//                                                                  ==================
+// -----------------------------------------------------
+//                                            Bean Field
+//                                            ----------
 // test of #9 Limit the generation target of property of bean of Param and return.
 // @Override
 remoteApiRule.targetField = function(api, topLevelBean, jsonFieldName) {
@@ -25,14 +17,23 @@ remoteApiRule.targetField = function(api, topLevelBean, jsonFieldName) {
 // test of #5 Derived from swagger field name to java field name (add @SerializedName) 
 // @Override
 remoteApiRule.fieldName = function(api, topLevelBean, jsonFieldName) {
-  // いったん元のサンプルを維持するために、bind(this) したりしなかったり
+  // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // ここでの super 呼び出しの前提:
+  // o ストレートに super を呼び出した場合: キャメル変換される
+  // o bind(this) で super を呼び出した場合: キャメル変換されない
+  //   (custom rule側で fieldNamingMapping() を空っぽオーバーライドしているから)
+  //
+  // これらをテストケース (url) によって使い分ける。
+  // _/_/_/_/_/_/_/_/
   if (api.url.contains('/ngchar/')) {
     // アスタリスクやハイフンなどの変数名で使えないものを_にしたり、先頭の数字をアルファベットに変えたり。
     var customName = jsonFieldName.replace(/[*-]/, '_').replace(/^1/, 'one');
-    return baseRule.fieldName(api, topLevelBean, customName);
+    return baseRule.fieldName(api, topLevelBean, customName); // superのまま呼んでキャメル変換もする
   } else if (api.url.contains('/noconv/')) {
-    // ケース混在のケースなので、キャメル変換させずまんまの形で表現させるために super 側を呼ばない。
-    return jsonFieldName;
+    // ケース混在のケースなので、キャメル変換させずまんまの形で表現させるために、
+    // bind(this)によりcustom rule側の fieldNamingMapping() 呼ぶようにする。
+    // (custom rule側では、無変換を実現するオーバーライドをしていること前提)
+    return baseRule.fieldName.bind(this)(api, topLevelBean, jsonFieldName);
 
   // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   // 数字キャメル問題: sea_land_1 が seaLand1 になって、Gsonの逆キャメルで sea_land1 になっちゃう。
@@ -42,25 +43,46 @@ remoteApiRule.fieldName = function(api, topLevelBean, jsonFieldName) {
     var customName = jsonFieldName
         .replace(/1/, 'one').replace(/2/, 'two').replace(/3/, 'three').replace(/4/, 'four').replace(/5/, 'five')
         .replace(/6/, 'six').replace(/7/, 'seven').replace(/8/, 'eight').replace(/9/, 'nine');
-    return baseRule.fieldName(api, topLevelBean, customName);
+    return baseRule.fieldName(api, topLevelBean, customName); // superを呼んでキャメル変換もする
   } else if (api.url.contains('/numbercamel/') && api.httpMethod === 'post') {
     // 回避策2: ケース変換しない、Java上でスネークケースのインスタンス変数名を許容する。
     // Lasta RemoteApi の LaJsonSender の JsonMappingOption にて、FieldNaming を IDENTITY にすること。
     // 未指定でもデフォルトでは IDENTITY になるが、アプリ側でデフォルトを変えている可能性もあるので。
-    return jsonFieldName.replace(/^3/, 'three'); // 先頭数字だけアルファベット変換 (テストケースとしてたまたま)
+    var customName = jsonFieldName.replace(/^3/, 'three'); // 先頭数字だけアルファベット変換 (テストケースとしてたまたま)
+    return baseRule.fieldName.bind(this)(api, topLevelBean, customName); // キャメル変換しないようにbind
   } else if (api.url.contains('/numbercamel/') && api.httpMethod === 'put') {
     // まんまキャメルケースにするとこうなるのパターン。
     // #for_now jflute 回避策3何かあるかな？ (2026/04/02)
     var customName = jsonFieldName.replace(/^3/, 'three'); // 先頭数字だけアルファベット変換 (テストケースとしてたまたま)
-    return baseRule.fieldName(api, topLevelBean, customName)
+    return baseRule.fieldName(api, topLevelBean, customName) // superを呼んでキャメル変換もする
   } else {
-    // #for_now jflute 元のテストが bind(this) で、super側のキャメル変換を抑制していたのでとりあえずそのまま (2026/04/02)
+    // デフォルトでは custom rule側の fieldNamingMapping を呼び出すてキャメル変換なしにする。
+    // (ここはどっちでもいい。テストケースの都合でまた変えるかも) (2026/04/07)
     return baseRule.fieldName.bind(this)(api, topLevelBean, jsonFieldName);
   }
 }
 
+
+// -----------------------------------------------------
+//                                             Nest Bean
+//                                             ---------
 // test of suffix hell e.g. result result headache
 remoteApiRule.nestClassName = function(api, className) {
   // For example, remote side uses Spring Framework
   return className.replace(/(Part|Model|Bean)$/, '') + 'Part';
+}
+
+
+// ===================================================================================
+//                                                                              Option
+//                                                                              ======
+// test of no case conversion
+// @Override
+remoteApiRule.fieldNamingMapping = function() {
+  // キャメルケース変換を一切しないのをcustom rule側のデフォルトとする。
+  // fieldName() のオーバーライドにて、super の fieldName() を呼ばないようにするやり方だと、
+  // super側にキャメルケースとは別の変換処理が入った時に追従できないので、
+  // あくまで fieldNamingMapping() のオーバーライドで無変換を表現するパターン。
+  return { // empty means no conversion at all parts
+  };
 }
